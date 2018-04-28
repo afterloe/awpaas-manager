13472e7b31fa055186b17003029bb79b6fb7274f
afterloe <lm6289511@gmail.com>
2018-04-28 14:39:59 +0800

    feat: map功能替换do

--- services/database/fsRegistry/SaveFile.go
+++ services/database/fsRegistry/SaveFile.go
@@ -1,25 +1,30 @@
 package fsRegistry
 
 import (
-	"../../../domain"
 	"../../../integrate/logger"
 	"../../../dao"
 	"reflect"
 	"fmt"
+	"time"
 )
 
 /**
 	保存数据到数据库
  */
-func SaveUploadInfo(info *domain.UploadFileInfo) (*domain.UploadFileInfo, error) {
+func SaveUploadInfo(info map[string]interface{}) (map[string]interface{}, error) {
+	baseInfo, ok := info["baseInfo"].(map[string]interface{})
+	if !ok {
+		baseInfo = map[string]interface{}{"CreateTime":time.Now().Unix(), "Status": true}
+	}
 	result, err := dao.Insert(priv_INSERT, [][]interface{}{
-		{nil, info.Name, info.FileType, info.UploadName, info.Size},
+		{nil, info["Name"], info["FileType"], info["UploadName"], info["Size"], info["Group"], baseInfo["UpdateTime"]
+			, baseInfo["CreateTime"], baseInfo["Status"]},
 	})
 	if nil != err {
 		return nil, err
 	}
 	val := reflect.ValueOf(result[0])
-	info.Id = val.MethodByName("LastInsertId").Call(nil)[0].Int()
-	logger.Info(fmt.Sprintf("insert db use SaveUploadInfo. id is %d", info.Id))
+	info["id"] = val.MethodByName("LastInsertId").Call(nil)[0].Int()
+	logger.Info(fmt.Sprintf("insert db use SaveUploadInfo. id is %d", info["id"]))
 	return info, nil
 }
